8de53f6fa0de00df83cbd1ce29f75ecc12314f69
FatihBc <fatihbuyukcam@gmail.com>
2026-02-18 20:40:49 +0300

    i18n: translate NotFoundPage and PersonalHistory, add TR history text

--- src/components/AboutPage/PersonalHistory.tsx
+++ src/components/AboutPage/PersonalHistory.tsx
@@ -1,10 +1,11 @@
-import historyData from "../../data/history";
+import { useTranslation } from "react-i18next";
 
 function PersonalHistory() {
+  const { t } = useTranslation();
   return (
     <div>
       <p className="leading-relaxed text-justify indent-8 py-4!">
-        {historyData.history}
+        {t("about.history")}
       </p>
     </div>
   );
